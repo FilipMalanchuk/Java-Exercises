797e08118fcbd35120dd83d4f10c33446a99936e
itkrab <krabanium@gmail.com>
2022-02-23 13:09:10 +0200

    testing packages

--- InputOutput/src/InputOutput.java
+++ InputOutput/src/InputOutput.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+import java.math.*;
+
+import test.test;
+
 
 public class InputOutput {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		//exN1();
+		test.testing(args);
 
+	}
+	
+	public static void exN1 () {
+		System.out.println("Задание №1");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Введите a");
+		double a = scanner.nextDouble();
+		scanner.nextLine();
+		System.out.println("Введите b");
+		double b = scanner.nextDouble();
+		scanner.nextLine();
+		
+		double c = Math.hypot(a, b);
+		
+		System.out.println("answer N1 = " + c);
 	}
 
 }
---
+++ InputOutput/src/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+
+public class test {
+
+	public static void testing(String[] args) {
+		System.out.println("123");
+
+	}
+
+}
